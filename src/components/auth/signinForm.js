25dcd239a84247741df76e04c0a3a1dfe52153c2
Jessie Galbraith <galbraithjessie@gmail.com>
2020-05-14 12:05:49 -0600

    Styled formFields
    Styled formFields scss files to align sign in form.

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -11,6 +11,7 @@ class SignInForm extends Component {
             <form onSubmit={handleSubmit} className={`${className}sign-in-form`}>
                 <Field className='sign-in-form__email' name='email' type='email' title='Email' placeholder='Email' component={FormInput}/>
                 <Field className='sign-in-form__password' name='password' type='password' title='Password' placeholder='Password' component={FormInput}/>
+                <div className='sign-in-form__line'></div>
                 <Field className='sign-in-form__login' onClick={() => console.log('tryna submit')} name='login' type='submit' title='Login'  component={FormButton}/>
             </form>
         )
