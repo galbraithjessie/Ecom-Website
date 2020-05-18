b090d1c2231e785fdcc485effe98982269cc859b
galbraithjessie <galbraithjessie@gmail.com>
2020-05-18 13:32:37 -0600

    Built components
    Built base structure for account components.

---
+++ src/components/account/accountInformation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AccountInformation extends Component {
+    render() {
+        return (
+            <div>
+                Account Info
+            </div>
+
+        )
+    }
+}
+
+export default AccountInformation;
---
+++ src/components/account/puchaseHistory.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PurchaseHistory extends Component {
+    render() {
+        return (
+            <div>
+                Purchase History
+            </div>
+
+        )
+    }
+}
+
+export default PurchaseHistory;
