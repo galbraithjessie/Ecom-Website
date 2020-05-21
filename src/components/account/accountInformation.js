722f6a020cbe2bf9ba7828aaeb7d449a72623067
Jessie Galbraith <galbraithjessie@gmail.com>
2020-05-21 12:40:04 -0600

    Built Account Info Form
    built account information form from sign in form template

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
+import PageTitle from '../pageTitle';
+import AccountInformationForm from './accountInformationForm';
+
 class AccountInformation extends Component {
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
-            <div>
-                Account Info
+            <div className='account-information'>
+                <PageTitle className='accoun-information' title='Account Information'/>
+                <AccountInformationForm onSubmit={this.onSubmit} className='account-information__form' />
             </div>
-
         )
     }
 }
