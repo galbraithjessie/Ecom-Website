641d962d3b26857f61df947706eeb6fc78630117
Jessie Galbraith <galbraithjessie@gmail.com>
2020-05-24 11:20:07 -0600

    Built Shop base
    Built the base for the shop component and set up the route.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -14,6 +14,7 @@ import Layout from "./components/layout";
 import SignIn from "./components/auth/signin";
 import SignUp from "./components/auth/signup";
 import Account from './components/account/account';
+import Shop from './components/shop/shop';
 
 function main() {
   ReactDOM.render(
@@ -26,6 +27,8 @@ function main() {
             <Route path='/signup' exact component={SignUp} />
 
             <Route path='/account' exact component={Account} />
+
+            <Route path='/shop' exact component={Shop} />
 
           </Switch>
         </Layout>
