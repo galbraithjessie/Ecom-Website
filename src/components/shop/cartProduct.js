320c898f774ffcd1975201b4ef7d5aeceeccdd1f
Jessie Galbraith <galbraithjessie@gmail.com>
2020-06-04 12:18:38 -0600

    Built CartProduct
    Built the CartProduct Component

---
+++ src/components/shop/cartProduct.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Quantity from '../quantity';
+import GreenPriceTag from '../greenPriceTag';
+ 
+export default class CartProduct extends Component {
+   render() {
+       const { className, title, quantity, price } = this.props;
+       return(
+          <div className={`${className} cart-product`}>
+              <img className='cart-product__image' src='http://via.placeholder.com/130x130' />
+              <div className='car-product__title'>{title}</div>
+              <Quantity className='cart-product__quantity' quantity={quantity}/>
+              <a className='cart-product__remove'>Remove</a>
+              <GreenPriceTag className='cart-product__price' title={price} />
+          </div>
+       );
+   }
+}
