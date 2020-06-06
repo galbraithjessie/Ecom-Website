829364ba61d38ba99721174a37b8bc6ca8ac25f0
Jessie Galbraith <galbraithjessie@gmail.com>
2020-06-06 11:23:02 -0600

    Setup cartProduct model
    Setup cartProduct model and action creator to fetch and set user cart products.

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,6 +1,7 @@
-import {
+import { 
     SET_USER_PURCHASES,
-    SET_PURCHASE_DETAIL
+    SET_PURCHASE_DETAIL,
+    SET_CART_PRODUCTS
 } from './types';
 
 export function setPurchaseDetail(_id) {
@@ -10,13 +11,44 @@ export function setPurchaseDetail(_id) {
     })
 }
 
+
+export function fetchCartProducts() {
+    return ({
+        type: SET_CART_PRODUCTS,
+        payload: [
+            {
+                _id: 0,
+                product: {
+                    _id: 0,
+                    title: 'JavaScript in the Browser',
+                    description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
+                    price: 1.99,
+                    belongsTo: [0, 1]
+                },
+                quantity: 2
+            },
+            {
+                _id: 1,
+                product: {
+                    _id: 1,
+                    title: 'Graph Database',
+                    description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
+                    price: 1.99,
+                    belongsTo: [0, 6]
+                },
+                quantity: 1
+            },
+        ]
+    })
+}
+
 export function fetchUserPurchases() {
     return ({
         type: SET_USER_PURCHASES,
         payload: [
             {
                 _id: 0,
-                total: 8.02,
+                total: 19.40,
                 orderNumber: 'A0048248343',
                 orderDate: new Date().toDateString(),
                 creditCard: '-0000',
@@ -27,19 +59,19 @@ export function fetchUserPurchases() {
             },
             {
                 _id: 1,
-                total: 19.40,
-                orderNumber: 'A00482483432',
+                total: 1.30,
+                orderNumber: '24343sdfkjh72',
                 orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
-                    name: 'Jordan Hudgens',
-                    shippingAddress: '1234 West State Street'
+                    name: 'Maxwell Nelson',
+                    shippingAddress: '1337 West State Street'
                 }
             },
             {
                 _id: 2,
-                total: 9.00,
-                orderNumber: 'A00482483433',
+                total: 1.30,
+                orderNumber: 'B0048248343',
                 orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
@@ -49,19 +81,19 @@ export function fetchUserPurchases() {
             },
             {
                 _id: 3,
-                total: 1.30,
-                orderNumber: 'A00482483434',
+                total: 16.40,
+                orderNumber: 'ADFasdlfkjwe',
                 orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
-                    name: 'Jordan Hudgens',
+                    name: 'Max Asher Nelson',
                     shippingAddress: '1234 West State Street'
                 }
             },
             {
                 _id: 4,
-                total: 10.80,
-                orderNumber: 'A00482483435',
+                total: 19.40,
+                orderNumber: 'A0048248343',
                 orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
@@ -71,8 +103,8 @@ export function fetchUserPurchases() {
             },
             {
                 _id: 5,
-                total: 7.20,
-                orderNumber: 'A00482483436',
+                total: 19.40,
+                orderNumber: 'A0048248343',
                 orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
@@ -82,8 +114,8 @@ export function fetchUserPurchases() {
             },
             {
                 _id: 6,
-                total: 6.80,
-                orderNumber: 'A00482483438',
+                total: 19.40,
+                orderNumber: 'A0048248343',
                 orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
@@ -93,15 +125,16 @@ export function fetchUserPurchases() {
             },
             {
                 _id: 7,
-                total: 3.25,
-                orderNumber: 'A00482483439',
+                total: 3.20,
+                orderNumber: '5UFISHDFJASUSHI298',
                 orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
-                    name: 'Jordan Hudgens',
-                    shippingAddress: '1234 West State Street'
+                    name: 'Andrew Wold',
+                    shippingAddress: '348 Yo State Street'
                 }
-            }
+            },
+          
         ]
     })
 }
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,25 +1,32 @@
 import {
     SET_USER_PURCHASES,
-    SET_PURCHASE_DETAIL
- } from '../actions/types';
+    SET_PURCHASE_DETAIL,
+    SET_CART_PRODUCTS
+} from '../actions/types';
 
- const INTIAL_STATE = {
-     purchases: [],
-     purchaseDetail: {
-         _id: -1,
-         total: 0,
-         orderNumber: '',
-         orderDate: null,
-         creditCard: '',
-         user: {
-             name: '',
-             shippingAddress: ''
-         }
-     }
- }
+const INITIAL_STATE = {
+    cartProducts: [],
+    purchases: [],
+    purchaseDetail: {
+        _id: -1,
+        total: 0,
+        orderNumber: '',
+        orderDate: null,
+        creditCard: '',
+        user: {
+            name: '',
+            shippingAddress: ''
+        }
+    }
+}
 
- export default function(state = INTIAL_STATE, action) {
+export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
+        case SET_CART_PRODUCTS:
+            return {
+                ...state,
+                cartProducts: action.payload
+            }
         case SET_USER_PURCHASES:
             return {
                 ...state,
@@ -38,4 +45,4 @@ import {
             }
         default: return state;
     }
- }
+}
