cd0161091b0598c3f656bd55e3989a9c4b15a538
Jessie Galbraith <galbraithjessie@gmail.com>
2020-06-06 11:47:57 -0600

    Finished Styling Cart
    Finished Styling Cart and fixed a syntax error

--- src/components/shop/cartProduct.js
+++ src/components/shop/cartProduct.js
@@ -9,7 +9,7 @@ export default class CartProduct extends Component {
        return(
           <div className={`${className} cart-product`}>
               <img className='cart-product__image' src='http://via.placeholder.com/130x130' />
-              <div className='car-product__title'>{title}</div>
+              <div className='cart-product__title'>{title}</div>
               <Quantity className='cart-product__quantity' quantity={quantity}/>
               <a className='cart-product__remove'>Remove</a>
               <GreenPriceTag className='cart-product__price' title={price} />
