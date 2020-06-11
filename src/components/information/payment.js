5cc093a818b8adfb80950bdc091f5f0e36698a34
Jessie Galbraith <galbraithjessie@gmail.com>
2020-06-11 11:02:33 -0600

    Set up Routes
    Built the information routes and container components for shipping and payment.

---
+++ src/components/information/payment.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+
+import PageTitle from '../pageTitle';
+
+//REDUX
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Payment extends Component {
+
+    componentDidMount() {
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([]);
+    }
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
+    render() {
+        return (
+            <div className='payment'>
+                <PageTitle className='payment__page-title' title='Payment Information' />
+                {/* <PaymentForm onSubmit={this.onSubmit} className='sign-in__form' /> */}
+            </div>
+        )
+    }
+}
+
+Payment = connect(null, actions)(Payment);
+
+export default Payment;
